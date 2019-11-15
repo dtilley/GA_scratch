5b4094b2aef58fb6adc5184afe7e5feed6c82fb8
dtilley <drew.tilley@gmail.com>
2019-11-14 20:01:27 -0500

    finished tutorial

--- one_max.py
+++ one_max.py
@@ -24,3 +24,45 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 
 def main():
     pop = toolbox.population(n=300)
+
+    # evlauate population fitness
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = (fit,)
+    CXPB, MUTPB = 0.5, 0.2
+    fits = [ind.fitness.values[0] for ind in pop]
+    g = 0
+    while max(fits) < 100 and g < 1000:
+        # A new generation
+        g = g + 1
+        print("-- Generation %i --" % g)
+        # create a list of the winners of 300 tornaments of size 3
+        # the breeding population #
+        offspring = toolbox.select(pop, len(pop))
+        offspring = list(map(toolbox.clone, offspring)) # deep.copy
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values            
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = (fit,)
+        pop[:] = offspring
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
